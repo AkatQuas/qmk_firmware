6462b12319b438c6fdd9fccc6c735612c591e564
AkatQuas <295140755@qq.com>
2024-03-11 10:58:17 +0800

    feat(annepro2): c18 & dvorak mappings

--- keyboards/annepro2/c18/rules.mk
+++ keyboards/annepro2/c18/rules.mk
@@ -17,14 +17,14 @@ PROGRAM_CMD = annepro2_tools --boot $(BUILD_DIR)/$(TARGET).bin
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
+MOUSEKEY_ENABLE = yes        # Mouse keys
+EXTRAKEY_ENABLE = no       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 
 # Custom RGB matrix handling
-RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_ENABLE = no
 
 # Keys
 CUSTOM_MATRIX = lite
---
+++ keyboards/annepro2/keymaps/dvorak/README.md
@@ -0,0 +1,18 @@
+> JSON files for http://www.keyboard-layout-editor.com .
+
+We have four layers.
+
+Default
+
+![](./keyboard-layout-default.png)
+
+Tap
+
+![](./keyboard-layout-tap.png)
+
+Fn1
+
+![](./keyboard-layout-fn1.png)
+Fn2
+
+![](./keyboard-layout-fn2.png)
